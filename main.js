11a6fa60d946da035e0c928227e77134de6a8100
Jesús Monge <jesus_jaml@outlook.com>
2022-04-02 16:49:14 -0500

    Added interactivity with js

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const $shoppingButton = document.querySelector('.shopping-cart-link')
 const $cartContainer = document.querySelector('.shopping-cart-container')
 const $leftArrowButton = document.querySelector('.left-arrow')
+const $produtCard = document.querySelectorAll('.product-card')
+const $mobileMenuIcon = document.querySelector('.header__menu-icon')
+const $mobileMenu = document.querySelector('.menu-mobile')
 
 // When the user click the shopping cart button show the shopping details
 $shoppingButton.addEventListener('click', () => {
@@ -14,4 +17,35 @@ const openCartDetails = () => {
 
 $leftArrowButton.addEventListener('click', () => {
   openCartDetails()
+})
+
+// Show mobile menu when clicked
+const showMobileMenu = () => {
+  console.log('first')
+  $mobileMenu.classList.toggle('menu-mobile--show')
+}
+
+$mobileMenuIcon.addEventListener('click', () => {
+  showMobileMenu()
+})
+
+// Change Icon source img when the click it
+$produtCard.forEach((e) => {
+  let $addToCartIcon = e.querySelector('.cart-icon-img')
+  $addToCartIcon.addEventListener('click', () => changeShoppingCartStatus(e))
+  function changeShoppingCartStatus(e) {
+    let $removedFromCart = e.querySelector('.removed-from-card')
+    shoppinIconSrc = $addToCartIcon.getAttribute('src')
+    console.log(shoppinIconSrc)
+    console.log($removedFromCart)
+
+    //* Using ES6 ternary operator
+    shoppinIconSrc !== './assets/add_to_cart.svg'
+      ? ($addToCartIcon.src = './assets/add_to_cart.svg') &&
+        ($removedFromCart.innerHTML = 'Removed from cart') &&
+        $removedFromCart.classList.toggle('removed-from-card--show')
+      : ($addToCartIcon.src = './assets/selected-to-buy.svg') &&
+        ($removedFromCart.innerHTML = 'Added to cart') &&
+        $removedFromCart.classList.toggle('removed-from-card--show')
+  }
 })
